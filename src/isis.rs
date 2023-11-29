a9021f80344165bb7ec1cc1af2cbbb0c9ed4fe8e
nrybowski <nicolas.rybowski@uclouvain.be>
2023-11-29 13:04:34 +0100

    Parse ExtendedIsReachability SubTLVs

--- src/isis.rs
+++ src/isis.rs
@@ -1,11 +1,14 @@
 use nom_derive::*;
-use nom::combinator::complete;
+use nom::error::{make_error, ErrorKind, Error};
+use nom::number::streaming::{be_u8, be_u16, be_u32};
+use nom::combinator::{complete, peek};
+use nom::bytes::streaming::take;
 use nom::multi::many0;
 pub use nom::IResult;
 
 use rusticata_macros::newtype_enum;
 
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
 #[derive(Clone, Copy, Debug, Default, Eq, PartialEq, NomBE)]
 pub struct IsisPacketType(pub u8);
@@ -41,22 +44,67 @@ pub struct IsisPacketHeader {
     pub max_area_adr: u8
 }
 
-#[derive(Nom)]
-#[derive(Debug)]
-#[nom(Selector(u8))]
-pub enum IsisTlv {
-    #[nom(Selector(22))]
-    TlvExtendedIsReachability(IsisTlvExtendedIsReachability),
-    #[nom(Selector(_), Ignore)]
-    Unsupported
-}
-
-#[derive(Debug)]
-pub enum IsisSubTlv {
-    Unsupported    
+fn skipper(input: &[u8]) -> IResult<&[u8], IsisTlv> {
+        let (_, hdr) = peek(be_u16)(input)?;
+        let len = hdr as u8;
+        let (rem, _skipped) = take::<u8, &[u8], Error<&[u8]>>((len+2) as u8)(input)?;    
+        Ok((rem, IsisTlv::Unsupported))
 }
 
 #[derive(Debug, NomBE)]
+#[nom(Selector(IsisTlvType))]
+pub enum IsisTlv {
+    #[nom(Selector(IsisTlvType::ExtendedIsReachability))]
+    TlvExtendedIsReachability(IsisTlvExtendedIsReachability),
+    #[nom(Selector(_), Parse(skipper))]
+    Unsupported,
+}
+
+fn parse_isis_tlvs<'a>(len: u16) -> impl Fn(&'a[u8]) -> IResult<&'a[u8], Vec<IsisTlv>> {
+    // println!("Called");
+    move |input: &'a[u8]| {
+        // println!("TlvS parse {:#x?} {} {}", input, len, input.len());
+        let (data, rem) = input.split_at(len.into());
+        let (_, v) = many0(complete(
+            move |input: &'a[u8]| {
+                // println!("Tlv parse {:#x?}", input);
+                let (_, t) = peek(be_u8)(input)?;
+                let t = IsisTlvType(t);
+                // println!("{:#?}", t);
+                IsisTlv::parse(input, t)
+            }
+        ))(data)?;
+        Ok((rem, v))
+    }
+}
+#[derive(Debug, Clone, NomBE)]
+pub struct IsisSubTlvIpv4InterfaceAddress {
+    #[nom(Verify = "header.tlv_type == 6")]
+    pub header: IsisTlvHeader,
+    #[nom(Map(|a| std::net::Ipv4Addr::from(a)), Parse(be_u32))]
+    pub addr: std::net::Ipv4Addr,
+}
+
+#[derive(Debug, Clone, NomBE)]
+pub struct IsisSubTlvIpv4NeighborAddress {
+    #[nom(Verify = "header.tlv_type == 8")]
+    pub header: IsisTlvHeader,
+    #[nom(Map(|a| std::net::Ipv4Addr::from(a)), Parse(be_u32))]
+    pub addr: std::net::Ipv4Addr,
+}
+
+#[derive(Debug, Clone, NomBE)]
+#[nom(Selector(IsisSubTlvType))]
+pub enum IsisSubTlv {
+    #[nom(Selector(IsisSubTlvType::Ipv4InterfaceAddress))]
+    Ipv4InterfaceAddress(IsisSubTlvIpv4InterfaceAddress),
+    #[nom(Selector(IsisSubTlvType::Ipv4NeighborAddress))]
+    Ipv4NeighborAddress(IsisSubTlvIpv4NeighborAddress),
+    #[nom(Selector(_), Ignore)]
+    Unsupported    
+}
+
+#[derive(Debug, Clone, NomBE)]
 pub struct IsisTlvHeader {
     pub tlv_type: u8,
     pub len: u8,
@@ -73,19 +121,40 @@ fn parse_is_neighbors(len: u8) -> impl Fn(&[u8]) -> IResult<&[u8], Vec<IsNeighbo
 
 #[derive(Debug, NomBE)]
 pub struct IsisTlvExtendedIsReachability {
+    #[nom(Verify = "header.tlv_type == 22")]
     pub header: IsisTlvHeader,
     #[nom(Parse(parse_is_neighbors(header.len)))]
     pub neighbors: Vec<IsNeighbor>,
 }
 
-#[derive(Debug, NomBE)]
+// IsisSubTlv::parse(input: &[u8], selector: u8) -> IResult<&[u8], IsisSubTlv>
+
+fn parse_isis_subtlvs<'a>(len: u8) -> impl Fn(&'a[u8]) -> IResult<&'a[u8], Vec<IsisSubTlv>> {
+    move |input: &'a[u8]| {
+        let (data, rem) = input.split_at(len.into());
+        // println!("SubTlvS parse {:#x?}", input);
+        let (_, v) = many0(complete(
+            move |input: &'a[u8]| {
+                // println!("SubTlv parse {:#x?}", input);
+                let (_, t) = peek(be_u8)(input)?;
+                let t = IsisSubTlvType(t);
+                // println!("{:#?}", t);
+                IsisSubTlv::parse(input, t)
+            }
+        ))(data)?;
+        Ok((rem, v))
+    }
+}
+
+#[derive(Debug, NomBE, Clone)]
 pub struct IsNeighbor {
     pub is_neighbor_id: [u8; 7],
     pub metric: [u8; 3],
     pub sub_tlvs_len: u8,
     // TODO: Handle SubTLVs
-    #[nom(SkipBefore(sub_tlvs_len), Ignore)]
-    pub sub_tlvs: Vec<IsisSubTlv>,
+    //#[nom(SkipBefore(sub_tlvs_len), Ignore)]
+    #[nom(Cond(sub_tlvs_len > 0), Parse(parse_isis_subtlvs(sub_tlvs_len)))]
+    pub sub_tlvs: Option<Vec<IsisSubTlv>>,
 }
 
 #[derive(PartialOrd, Ord, Eq, Hash, PartialEq, Clone, Copy, NomBE, Serialize)]
@@ -129,8 +198,8 @@ pub struct IsisL2LinkStatePacket {
     pub seq_no: SeqNo,
     pub checksum: Chksm,
     pub type_block: u8,
-    // #[nom(Cond(pdu_length - 19 > 0))]
-    pub tlvs: Vec<IsisTlv>,
+    #[nom(Cond(pdu_length - 27 > 0), Parse(parse_isis_tlvs(pdu_length - 27)))]
+    pub tlvs: Option<Vec<IsisTlv>>,
 }
 
 #[derive(Clone, Copy, Debug, Default, Eq, PartialEq, NomBE)]
@@ -147,5 +216,7 @@ pub struct IsisSubTlvType(pub u8);
 
 newtype_enum! {
     impl display IsisSubTlvType {
+        Ipv4NeighborAddress = 8,
+        Ipv4InterfaceAddress = 6,
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -23,57 +23,6 @@ pub fn parse_isis_packet(input: &[u8]) -> IResult<&[u8], IsisPacket> {
     }
 }
 
-// TODO: Use only nom_derive Selector if possible
-impl <'a> Parse<&'a [u8]> for IsisTlv {
-      fn parse(input: &[u8]) -> IResult<&[u8], IsisTlv> {
-        let (_, word) = peek(be_u16)(input)?;
-        let tlv_type = (word >> 8) as u8;
-        let len = word as u8;
-        // println!("{:x?}", input);
-        match IsisTlvType(tlv_type) {
-            IsisTlvType::ExtendedIsReachability => {
-                let ret = map(
-                        IsisTlvExtendedIsReachability::parse,
-                        IsisTlv::TlvExtendedIsReachability
-                    )(input);
-                // println!("{:#x?}", ret);
-                ret
-            },
-            _ => {
-                if len == 0 {
-                    Err(nom::Err::Error(make_error(input, ErrorKind::Tag)))
-                } else {
-                    // println!("TLV unsuported: type <{}> len <{}>", tlv_type, len);
-                    let ret = take::<u8, &[u8], Error<&[u8]>>((len +2) as u8)(input).unwrap();
-                    // println!("TLV unsuported: type <{}> len <{}> buf <{:#x?}>", tlv_type, len, ret.1);
-                    Ok((ret.0, IsisTlv::Unsupported))
-                }
-            },
-        }
-    }
-}
-
-// impl <'a> Parse<&'a [u8]> for IsisSubTlv {
-//       fn parse(input: &[u8]) -> IResult<&[u8], IsisSubTlv> {
-//         let (_, word) = peek(be_u16)(input)?;
-//         let subtlv_type = (word >> 8) as u8;
-//         let len = word as u8;
-//         match IsisSubTlvType(subtlv_type) {
-//             _ => {
-//                 //if len == 0 {
-//                 //    Err(nom::Err::Error(make_error(input, ErrorKind::Tag)))
-//                 //} else {
-//                     // let ret = take::<u8, &[u8], Error<&[u8]>>((len +2) as u8)(input).unwrap();
-//                     // println!("TLV unsuported: type <{}> len <{}> buf <{:#x?}>", subtlv_type, len, ret.1);
-//                     println!("Sub TLV unsupported: type <{}> len <{}>", subtlv_type, len);
-//                     // Ok((ret.0, IsisSubTlv::Unsupported))
-//                     Ok((input, IsisSubTlv::Unsupported))
-//                 //}
-//             },
-//         }
-//     }
-// }
-
 #[cfg(test)]
 mod test {
     use crate::*;
